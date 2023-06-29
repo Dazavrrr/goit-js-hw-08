34563298b551973842a1e8e45ccb68192acf5cdd
Dazavrrr <damelasha@gmail.com>
2023-06-29 18:36:24 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryList = galleryItems
+  .map((item) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${item.original}">
+      <img 
+        class="gallery__image" 
+        src="${item.preview}" 
+        alt="${item.description}" 
+        />
+   </a>
+</li>`;
+  })
+  .join("");
+
+gallery.innerHTML = galleryList;
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  showCounter: false,
+  close: false,
+});
